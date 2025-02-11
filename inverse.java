19ea27a71f02de4907cd8d75337971240a87e71e
NavyaArora24 <aroranavya78@gmail.com>
2025-02-12 01:21:55 +0500

    changes done

---
+++ inverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class inverse {
+    
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n= scn.nextInt();
+        int i=0;
+        int op=1;
+        while(n>0){
+            int od=n%10;
+            int id=op;
+            int ip= od;
+        }
+        scn.close();
+    }
+}
