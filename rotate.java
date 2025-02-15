f9ea8f8f1903ea1fc9883b2f74bcbe3a3240441f
NavyaArora24 <aroranavya78@gmail.com>
2025-02-16 00:19:41 +0500

    changes updated

--- rotate.java
+++ rotate.java
@@ -5,10 +5,34 @@ public class rotate {
         Scanner scn= new Scanner(System.in);
         System.out.println("Enter a number:");
         int n=scn.nextInt();
+        System.out.println("Enter no of times it needs to be rotated");
         int rotate= scn.nextInt();
-        while(n>0){
-            int 
+        int temp=n;
+        int nod=0;
+        while(temp>0){
+            temp=temp/10;
+            nod++;
         }
+        if(rotate>nod){
+        rotate=rotate%nod;
+        }
+        if(rotate<0){
+            rotate=rotate+nod;
+        }
+        int div=1;
+        int mult=1;
+        for(int i=1;i<=nod; i++){
+            if(i<=rotate){
+                div=div*10;
+            }
+            else{
+                mult=mult*10;
+            }
+        }
+        int q=n/div;
+        int r=n%div;
+        int rem=r*mult+q;
+        System.out.println(rem);
         scn.close();
     }
 }
