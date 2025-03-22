e9b639f6e9d67b8c6984951ce8a86aa498ac552c
NavyaArora24 <aroranavya78@gmail.com>
2025-03-23 02:21:46 +0500

    change

---
+++ pattern16.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class pattern16 {
+    
+    public static void main(String[] args) {
+        Scanner scn= new Scanner(System.in);
+        int n=scn.nextInt();
+        int star=1;
+        int space= 2*n-3;
+        for(int i=1;i<=n;i++){
+            int val=1;
+            for(int j=1;j<=star;j++){
+                System.out.print(val+"\t");
+                if(j<star){
+                val++;
+                }
+                
+            }
+            for(int j=1;j<=space;j++){
+                System.out.print("\t");
+            }
+            if (i == n) { // Special case for the last row to avoid overlap
+                val--;
+            }
+            for(int j=1;j<=star;j++){
+                System.out.print(val+"\t");
+                if(j<star){
+                val--;
+                }
+            }
+            System.out.println();
+            star++;
+            space-=2;
+        }
+        scn.close();
+}
+}
