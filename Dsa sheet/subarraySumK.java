4337f6db65b899b5e6a283d087a0ecc6f9a184cc
NavyaArora24 <aroranavya78@gmail.com>
2025-06-10 03:11:07 +0500

    done

---
+++ Dsa sheet/subarraySumK.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+
+public class subarraySumK {
+    public static void main(String[] args) {
+        int[] arr = {10};
+        int k = 10;
+
+        int result = subarraySum(arr, k);
+        System.out.println(result);
+    }
+
+    // public static int subarraySum(int[] arr, int k){
+    //     int count = 0;
+    //     for(int i = 0; i< arr.length; i++){
+    //         int sum = 0;
+    //         for(int j = i; j< arr.length; j++){
+    //             sum += arr[j];
+    //             if(sum == k){
+    //                 count++;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
+
+    public static int subarraySum(int[] arr, int k){
+        int count =0;
+        HashMap <Integer,Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        for(int i = 0; i< arr.length; i++){
+            sum += arr[i];
+            if(map.containsKey(sum - k)){
+                count += map.get(sum - k);
+            }
+            if(map.containsKey(sum)){
+                map.put(sum, map.get(sum)+1);
+            }
+            else{
+                map.put(sum, 1);
+
+            }
+        }
+        return count;
+    }
+}
