fde05129fe8bbd66229c4a49f323c5009b9535c6
NavyaArora24 <aroranavya78@gmail.com>
2025-07-08 02:44:32 +0500

    done

---
+++ two_pointers/happyNumber.java
@@ -0,0 +1,33 @@
+package two_pointers;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class happyNumber {
+    public static void main(String[] args) {
+        int n = 19;
+
+        boolean result = isHappy(n);
+System.out.println(result);
+}
+public static boolean isHappy(int n) {
+        Set<Integer>seen = new HashSet<>();
+        while(n != 1 && !seen.contains(n)){
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+    public static int getNext(int n){
+        int sum =0;
+        while(n>0){
+            int digit = n%10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+
+    }
+}
